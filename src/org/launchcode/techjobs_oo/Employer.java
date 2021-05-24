8c54a459014375c45ad2d27307029b4bc2954d30
zabalav <vincent_zabala@yahoo.com>
2021-05-24 13:48:56 -0500

    Cleaned up Employer

--- src/org/launchcode/techjobs_oo/Employer.java
+++ src/org/launchcode/techjobs_oo/Employer.java
@@ -7,7 +7,7 @@ public class Employer extends JobField {
     }
 
     @Override
-    public boolean equals(Object o) {  // Two objects are equal if they have the same id.
+    public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof Employer)) return false;
         Employer employer = (Employer) o;
